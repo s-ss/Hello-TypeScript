fd73a184b61336150b8a725159c9a3ff3e1caed8
s-ss <sinz@no-real.com>
2016-05-29 21:14:02 +0900

    interface Person追加

--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,12 @@
-function greeter(person:string) {
-    return "Hello, " + person;
+interface Person {
+    firstName: string;
+    lastName: string;
 }
 
-var user = [0, 1, 2];
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+var user = { firstName: "Jane", lastName: "User" };
 
 document.body.innerHTML = greeter(user);
