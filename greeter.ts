cac06fc81bfe2d34e4d74434067122ff3ae17a05
s-ss <sinz@no-real.com>
2016-05-29 21:20:41 +0900

    Studentクラス追加

--- greeter.ts
+++ greeter.ts
@@ -1,3 +1,11 @@
+class Student {
+    fullName:string;
+
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
