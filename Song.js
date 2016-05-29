2709f0291e61687f0a3a63c882d41dbcdaabe0a1
s-ss <sinz@no-real.com>
2016-05-30 00:05:01 +0900

    生成物変更

--- Song.js
+++ Song.js
@@ -23,6 +23,20 @@ var Jukebox = (function () {
     };
     return Jukebox;
 }());
+var Playlist = (function () {
+    function Playlist(name) {
+        this.name = name;
+        this.songs = [];
+    }
+    Playlist.prototype.addSong = function (song) {
+        if (this.songs.length >= Playlist.macSongCount) {
+            throw new Error('Playlist is full');
+        }
+        this.songs.push(song);
+    };
+    Playlist.macSongCount = 30;
+    return Playlist;
+}());
 var songs = [
     new Song('Bushbaby', 'Megaphone'),
     new Song('Delays', 'One More Lie In'),
@@ -32,4 +46,8 @@ var songs = [
 ];
 var jukebox = new Jukebox(songs);
 jukebox.play();
+var playlist = new Playlist('My Playlist');
+var name = playlist.name;
+playlist.addSong(new Song('Therapy?', 'Crooked Timber'));
+var maxSongs = playlist.maxSongCount;
 //# sourceMappingURL=Song.js.map
