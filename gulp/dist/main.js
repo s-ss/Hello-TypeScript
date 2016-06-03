fd10b811462dcc4b3ef0b820625385f586cc111c
s-ss <sinz@no-real.com>
2016-06-04 00:08:44 +0900

    gulp sample

---
+++ gulp/dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+function hello(compiler) {
+    console.log("Hello from " + compiler);
+}
+hello("TypeScript");
+var greet_1 = require("./greet");
+console.log(greet_1.sayHello("TypeScript"));
